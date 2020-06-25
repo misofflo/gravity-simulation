402afb280f071a99b6a6c1a9ea620c2bff22f83e
misofflo <61591151+misofflo@users.noreply.github.com>
2020-06-25 12:34:54 +0200

    Floating point imprecision errors resolved and little rocket added.

--- GravitySimulation/Assets/Scripts/Constants.cs
+++ GravitySimulation/Assets/Scripts/Constants.cs
@@ -2,7 +2,7 @@
 
 public class Constants : MonoBehaviour {
     public float G = 0.0000000000667408f;
-    [Range(0, 1000000000000)]
+    [Range(0, 10000000)]
     public float gravMultiplier = 1000;
 	public float timescale = 1;
 
---
+++ GravitySimulation/Assets/Scripts/Movement Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Fields
+    private Camera playerCamera;
+    private Transform[] physicsObjects;
+    private Rigidbody rb;
+    private MassV3 gravity;
+
+    public float distanceThreshold = 1000;
+    [Range(1, 10000)]
+    public float forceMultiplier;
+
+    // Functions
+    // Start is called before the first frame update
+    private void Start() {
+        gravity = GetComponent<MassV3>();
+        rb = GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
+        physicsObjects = FindObjectsOfType<Transform>();
+    }
+
+	private void Update() {
+        UpdateFloatingOrigin();
+    }
+
+	private void FixedUpdate() {
+        MovePlayer();
+	}
+
+	private void UpdateFloatingOrigin() {
+        Vector3 originOffset = playerCamera.transform.position;
+        float dstFromOrigin = originOffset.magnitude;
+
+        if (dstFromOrigin > distanceThreshold) {
+            foreach (Transform t in physicsObjects) {
+                t.position -= originOffset;
+			}
+		}
+	}
+
+    private void MovePlayer() {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        transform.Translate(new Vector3(inputDir.x, 0, inputDir.y) * input.magnitude);
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            transform.Translate(transform.up * 1);
+		}
+	}
+}
