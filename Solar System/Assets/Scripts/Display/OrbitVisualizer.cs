868b3416f880afb77004a171fd4f5f527674b831
misofflo <61591151+misofflo@users.noreply.github.com>
2020-10-22 17:43:00 +0200

    Added multiple orbit visualization.

--- Solar System/Assets/Scripts/Display/OrbitVisualizer.cs
+++ Solar System/Assets/Scripts/Display/OrbitVisualizer.cs
@@ -4,18 +4,22 @@
 public class OrbitVisualizer : MonoBehaviour {
 	private CelestialObject[] allBodies;
 
-	[SerializeField]
-	private int selectedBodyIndex = 0;
-
 	private float timeStep = Universe.physicsTimeStep;
 	[SerializeField]
 	private int iterations = 1000;
 
+	[SerializeField]
+	private int focus = 0;
+
 	private void Update() {
-		DrawOrbit(selectedBodyIndex);
+		if (focus != 0) {
+			DrawOrbits(focus);
+		} else {
+			DrawOrbits();
+		}
 	}
 
-	private void DrawOrbit(int bodyIndex) {
+	private void DrawOrbits() {
 		allBodies = FindObjectsOfType<CelestialObject>();
 		VirtualBody[] allVirtualBodies = new VirtualBody[allBodies.Length];
 		Vector3[] drawPoint = new Vector3[iterations];
@@ -25,30 +29,62 @@ public class OrbitVisualizer : MonoBehaviour {
 			allVirtualBodies[i] = new VirtualBody(allBodies[i]);
 		}
 
+		foreach (VirtualBody body in allVirtualBodies) {
+			// simulate
+			for (int i = 0; i < iterations; i++) {
+				// updating velocity of selected body
+				body.UpdateVelocity(allVirtualBodies, timeStep);
+
+				// updating position
+				body.UpdatePosition(timeStep);
+				drawPoint[i] = body.position;
+			}
+
+			// draw orbit
+			for (int i = 0; i < iterations - 1; i++) {
+				Debug.DrawLine(drawPoint[i], drawPoint[i + 1], body.pathColor);
+			}
+		}
+	}
+
+	private void DrawOrbits(int index) {
+		allBodies = FindObjectsOfType<CelestialObject>();
+		VirtualBody[] allVirtualBodies = new VirtualBody[allBodies.Length];
+		Vector3[] drawPoint = new Vector3[iterations];
+
+		index -= 1;
+
+		// initialize virtual bodies
+		for (int i = 0; i < allBodies.Length; i++) {
+			allVirtualBodies[i] = new VirtualBody(allBodies[i]);
+		}
+
 		// simulate
-		for (int step = 0; step < iterations; step++) {
+		for (int i = 0; i < iterations; i++) {
 			// updating velocity of selected body
-			allVirtualBodies[bodyIndex].UpdateVelocity(allVirtualBodies, timeStep);
+			allVirtualBodies[index].UpdateVelocity(allVirtualBodies, timeStep);
 
 			// updating position
-			allVirtualBodies[bodyIndex].UpdatePosition(timeStep);
-			drawPoint[step] = allVirtualBodies[bodyIndex].position;
+			allVirtualBodies[index].UpdatePosition(timeStep);
+			drawPoint[i] = allVirtualBodies[index].position;
 		}
 
 		// draw orbit
 		for (int i = 0; i < iterations - 1; i++) {
-			Debug.DrawLine(drawPoint[i], drawPoint[i + 1], Color.red);
+			Debug.DrawLine(drawPoint[i], drawPoint[i + 1], allVirtualBodies[index].pathColor);
 		}
 	}
 
 	class VirtualBody {
 		public Vector3 position, velocity;
 		public float mass;
+		public Color pathColor;
 
 		public VirtualBody(CelestialObject body) {
 			position = body.transform.position;
 			velocity = body.GetVelocity();
 			mass = body.GetMass();
+			pathColor = body.gameObject.GetComponent<Renderer>().sharedMaterial.color;
 		}
 
 		public void UpdateVelocity(VirtualBody[] allBodies, float timeStep) {
